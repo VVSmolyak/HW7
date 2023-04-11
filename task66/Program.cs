4a8bfc499a399b9fa3445e74a4698ceffc3caeb5
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2023-04-11 22:35:56 +0300

    task66 added

---
+++ task66/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Type first number");
+int first = readInt();
+Console.WriteLine("Type second number");
+int second = readInt();
+
+int result = 0;
+if(first < second) {
+    result = sum(first, second);
+} else {
+    result = sum(second, first);
+}
+Console.WriteLine("===>" + result);
+
+int sum(int first, int second) {
+    int result = 0;
+    while(first < second + 1) {
+        result += first;
+        first++;
+    }
+    return result;
+}
+
+int readInt() {
+    try {
+        return int.Parse(Console.ReadLine());
+    } catch {
+        Console.WriteLine("Incorrect input");
+        return 0;
+    }
+}
