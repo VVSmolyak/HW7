fb0048b4c8780c4a97374108812cc5d73c7c3dcf
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2023-04-11 22:36:28 +0300

    task68 added

---
+++ task68/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Type first number");
+int first = readInt();
+Console.WriteLine("Type second number");
+int second = readInt();
+
+int result = ack(first, second);
+
+Console.WriteLine("===>" + result);
+
+int ack(int n, int m) {
+    if (n == 0) {
+        return m + 1;
+    } else if (m == 0) {
+        return ack(n -1, 1);
+    }
+    return ack(n - 1, ack (n, m - 1));
+}
+
+int readInt() {
+    try {
+        return int.Parse(Console.ReadLine());
+    } catch {
+        Console.WriteLine("Incorrect input");
+        return 0;
+    }
+}
