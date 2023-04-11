c6f25ffd6dfc2f917c193f87305a31ec08ac2901
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2023-04-11 22:35:13 +0300

    task64 added

---
+++ task64/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int number = readInt();
+printRec(number);
+
+void printRec(int number) {
+    if (number <= 0 ) {
+        return;
+    }
+    if (number == 1) {
+        Console.Write(number);
+    } else {
+        Console.Write(number + ", ");
+    }
+    printRec(number-1);
+}
+
+int readInt() {
+    try {
+        return int.Parse(Console.ReadLine());
+    } catch {
+        Console.WriteLine("Incorrect input");
+        return 0;
+    }
+}
